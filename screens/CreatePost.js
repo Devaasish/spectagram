7f76624c1ee2fee382b94a1a118c55ee6d4ee84f
Devaasish <75482391+Devaasish@users.noreply.github.com>
2021-11-24 20:31:30 +0530

    Create CreatePost.js

---
+++ screens/CreatePost.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class CreatePost extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Create Post</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
